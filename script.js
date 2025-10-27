473a477df7721da08e3d5df80013269b297c3329
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2025-10-27 00:21:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // ✅ Your actual Supabase credentials
 const supabaseUrl = "https://iebcbvlfdzudrinosshv.supabase.co";
-const supabaseKey = "YOUR_KEY_HERE"; // paste your anon key here ✅
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllYmNidmxmZHp1ZHJpbm9zc2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MDg5NTMsImV4cCI6MjA3NzA4NDk1M30.6-XwuwryT84txvbY-_T_e7FlzhexU_Bl75azzSmVJfA"; // paste your anon key here ✅
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const googleLoginBtn = document.getElementById("googleLogin");
